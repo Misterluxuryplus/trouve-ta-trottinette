6a08faef35a26b9fc270e867d7400e8543119964
Misterluxuryplus <sylv.citt38@gmail.com>
2026-05-17 09:18:41 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// Change only this value on each release: v1-12, v1-13, v1-14...
-const CACHE_NAME = "trouve-ta-trott-v1-12";
+// Change only this value on each release: v1-13, v1-14, v1-15...
+const CACHE_NAME = "trouve-ta-trott-v1-13";
 
 const APP_SHELL = [
   "./",
